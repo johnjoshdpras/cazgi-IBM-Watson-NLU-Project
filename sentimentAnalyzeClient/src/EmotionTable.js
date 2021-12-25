ef3f779cd9f87dac258ae08e92713d76499e259e
Joshua Dwi Prasetyo <artjo26@gmail.com>
2021-12-25 06:43:22 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -12,11 +12,11 @@ class EmotionTable extends React.Component {
                 /*Write code to use the .map method that you worked on in the 
               Hands-on React lab to extract the emotions. If you are stuck,
               please click the instructions to see how to implement a map*/
-                Object.entries(this.props.emotions).map(function(mapentry) {
+              this.props.emotions && Object.entries(this.props.emotions).map(function(element,val) {
                     return (
-                        <tr>
-                        <td>{mapentry[0]}</td>
-                        <td>{mapentry[1]}</td>
+                        <tr key={element}>
+                        <td>{element[0]}</td>
+                        <td>{element[1]}</td>
                         </tr>
                     )
                 })
